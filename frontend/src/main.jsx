695b3dd0e6ef4165e7fe72f9d943288e7760bb22
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2024-02-19 00:06:06 +0530

    frontend is created

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { createContext, useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+export const Context = createContext({
+  isAuthenticated: false,
+});
+
+const AppWrapper = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState({});
+  const [blogs, setBlogs] = useState([]);
+  const [mode, setMode] = useState("dark");
+
+  return (
+    <Context.Provider
+      value={{
+        user,
+        setUser,
+        blogs,
+        setBlogs,
+        mode,
+        setMode,
+        isAuthenticated,
+        setIsAuthenticated,
+      }}
+    >
+      <App />
+    </Context.Provider>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppWrapper />
+  </React.StrictMode>
+);
