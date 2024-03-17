a60c4ced7bd1eea36ca74d0cbd96626ad7b1bfbb
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2024-03-17 19:00:41 +0530

    file is deployment ready

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,9 @@ cloudinary.v2.config({
 
 
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
 
 app.listen(process.env.PORT, () => {
