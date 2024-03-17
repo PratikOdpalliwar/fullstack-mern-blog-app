ea2b6fd5fbb6f855becc3f7369085123669d0873
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2024-03-17 23:45:47 +0530

    test two commit

--- backend/app.js
+++ backend/app.js
@@ -1,59 +1,59 @@
-import express from "express";
-import path from "path";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import { dbConnection } from "./database/dbConnection.js";
-import { errorMiddleware } from "./middlewares/error.js";
-import userRouter from "./routes/userRouter.js";
-import blogRouter from "./routes/blogRouter.js";
-import fileUpload from "express-fileupload";
+// import express from "express";
+// import path from "path";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import { dbConnection } from "./database/dbConnection.js";
+// import { errorMiddleware } from "./middlewares/error.js";
+// import userRouter from "./routes/userRouter.js";
+// import blogRouter from "./routes/blogRouter.js";
+// import fileUpload from "express-fileupload";
 
-const app = express();
-dotenv.config({ path: "./config/config.env" });
+// const app = express();
+// dotenv.config({ path: "./config/config.env" });
 
-//for deployment
-const __dirname = path.resolve();
+// //for deployment
+// const __dirname = path.resolve();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "PUT", "DELETE", "POST"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [process.env.FRONTEND_URL],
+//     methods: ["GET", "PUT", "DELETE", "POST"],
+//     credentials: true,
+//   })
+// );
 
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: "/tmp/",
-  })
-);
+// app.use(
+//   fileUpload({
+//     useTempFiles: true,
+//     tempFileDir: "/tmp/",
+//   })
+// );
 
-app.use("/api/v1/user", userRouter);
-app.use("/api/v1/blog", blogRouter);
+// app.use("/api/v1/user", userRouter);
+// app.use("/api/v1/blog", blogRouter);
 
-dbConnection();
+// dbConnection();
 
-app.use(errorMiddleware);
+// app.use(errorMiddleware);
 
-//middlewar for deployment
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// //middlewar for deployment
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
 
-export default app;
+// export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,79 @@
-import app from "./app.js";
+// import app from "./app.js";
+// import cloudinary from "cloudinary";
+
+
+// cloudinary.v2.config({
+//   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+//   api_key: process.env.CLOUDINARY_CLIENT_API,
+//   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+// });
+
+
+
+// // app.get("/", (req, res) => {
+// //   res.send("Hello World");
+// // });
+
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on port ${process.env.PORT}`);
+// });
+
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { dbConnection } from "./database/dbConnection.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import userRouter from "./routes/userRouter.js";
+import blogRouter from "./routes/blogRouter.js";
+import fileUpload from "express-fileupload";
 import cloudinary from "cloudinary";
 
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+const __dirname = path.resolve();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "PUT", "DELETE", "POST"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/blog", blogRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
@@ -8,13 +81,9 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
 
+const PORT = process.env.PORT || 3000;
 
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
-
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
